c88ae682f3a83cc2dc9b90709458eb7bd19fe998
akhilan576 <akhilan576@gmail.com>
2026-04-04 16:12:44 +0530

    simpler cli

--- cli/bin/cortex.js
+++ cli/bin/cortex.js
@@ -6,6 +6,17 @@ program
   .name('cortex')
   .description('Cortex neuro-linter CLI')
   .version('1.0.0')
+  .argument('[file]', 'HTML/CSS file to evaluate')
+  .option('-i, --image <path>', 'Screenshot image (PNG/JPG)')
+  .option('--server <url>', 'Server URL', 'http://localhost:3001')
+  .option('-o, --output <path>', 'Write refactored code to file')
+  .action((file, opts) => {
+    if (!file) {
+      program.help()
+      return
+    }
+    evaluate({ file, ...opts })
+  })
 
 program
   .command('evaluate')
